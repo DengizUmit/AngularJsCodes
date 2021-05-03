b86720427042ff36922924e8b5108fd6f99a3d3a
Ümit Dengiz <umitdengiz98@gmail.com>
2021-05-03 15:00:48 +0300

    AngularJs - Directives

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 var myAngular = angular.module("myAngular",[]);
+
+myAngular.directive("testDirective", function(){
+    return {
+
+        // class >>restrict : "C"   -----  Restrictions ; E for Element, A for Attribute, C for Class, M for Element, default value : EA
+
+        // comment line
+        restrict : "M",
+        replace  : true,
+        template : "<h3>Create - AngularJs Directive</h3>"
+    };
+});
--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,23 @@
 myAngular.controller("myControl",function($scope){
+
     $scope.num1 = 3;
     $scope.num2 = 2;
+
     $scope.job = "Computer Engineer";
+
     $scope.person = { firstName : "Ümit", lastName : "Dengiz"};
+
     $scope.days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    $scope.quantity  = 0;
+    $scope.price = 0 ;
+
+    $scope.languages = ["HTML", "CSS", "JavaScript", "AngularJS", "RaeactJS", "VueJS", "NodeJS"];
+
+    $scope.languagesAll = [
+        {name : 'Ragnar', languages : 'AngularJS'},
+        {name : 'Lagertha', languages : 'RaeactJS'},
+        {name : 'Bjorn', languages : 'VueJS'},
+        {name : 'Atheltsan', languages : 'NodeJS'}
+    ];
 });
